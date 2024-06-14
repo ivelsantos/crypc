36b6daa5525aa9719bb7376cc5295f6726fbf8cc
Levi Santos <ivel.santos@proton.me>
2024-06-14 00:08:31 -0300

    refactoring

--- main.c
+++ main.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
-    FILE *fp;
-    int chr;
-    char *file_path;
+int check_arguments_lenght(int argc);
 
+int main(int argc, char *argv[]){
+    if(check_arguments_lenght(argc)){return EXIT_FAILURE;}
+
+    char *input_file_path = argv[1];
+    FILE *input_file;
+    int character;
+
+    input_file = fopen(input_file_path, "r");
+
+    if (input_file == NULL){
+        printf("Error opening the file!\n");
+        return EXIT_FAILURE;
+    }
+
+    while((character = fgetc(input_file)) != EOF){
+        int output_character = character + 1;
+        if (output_character > 126){
+            output_character = 33;
+        }
+        printf("%c", output_character);
+    }
+
+    printf("All good!\n");
+    fclose(input_file);
+    return EXIT_SUCCESS;
+}
+
+int check_arguments_lenght(int argc){
     if (argc > 2){
         printf("Too many arguments!\n");
         return EXIT_FAILURE;
@@ -13,19 +38,5 @@ int main(int argc, char *argv[]){
         printf("File path not provided!\n");
         return EXIT_FAILURE;
     } 
-
-    fp = fopen(argv[1], "r");
-
-    if (fp == NULL){
-        printf("Error opening the file!\n");
-        return EXIT_FAILURE;
-    }
-
-    while((chr = fgetc(fp)) != EOF){
-        printf("%c", chr);
-    }
-
-    printf("All good!\n");
-    fclose(fp);
-    return EXIT_SUCCESS;
+    return 0;
 }
