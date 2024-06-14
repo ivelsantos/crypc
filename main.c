175694d57d0b41570e9f6d11bfbbe714282fccf8
Levi Santos <ivel.santos@proton.me>
2024-06-13 23:13:37 -0300

    file opening

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-    printf("Starting from here!\n");
-    return 0;
+int main(int argc, char *argv[]){
+    FILE *fp;
+    int chr;
+    char *file_path;
+
+    if (argc > 2){
+        printf("Too many arguments!\n");
+        return EXIT_FAILURE;
+    } else if(argc < 2){
+        printf("File path not provided!\n");
+        return EXIT_FAILURE;
+    } 
+
+    fp = fopen(argv[1], "r");
+
+    if (fp == NULL){
+        printf("Error opening the file!\n");
+        return EXIT_FAILURE;
+    }
+
+    while((chr = fgetc(fp)) != EOF){
+        printf("%c", chr);
+    }
+
+    printf("All good!\n");
+    fclose(fp);
+    return EXIT_SUCCESS;
 }
