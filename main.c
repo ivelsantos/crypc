a5b33b0b62264ae3f1470745e8e0e9306e845c74
Levi Santos <ivel.santos@proton.me>
2024-06-16 17:09:40 -0300

    handling bad key input

--- main.c
+++ main.c
@@ -5,10 +5,21 @@
 int check_arguments_lenght(const int argc, const int lenght);
 
 int main(int argc, char *argv[]){
-    if(check_arguments_lenght(argc, 3)){return EXIT_FAILURE;}
+    if(check_arguments_lenght(argc, 4)){return EXIT_FAILURE;}
     char *input_file_path = argv[1];
     char *mode = argv[2];
-    if(subs_cipher_ascii(input_file_path, 10, mode)){
+    char *badchar;
+    /* char *key_char = argv[3]; */
+    int key = strtol(argv[3], &badchar, 10);
+
+    /* char *badchar; */
+    /* int key = strtol(key_char, &badchar, 10); */
+    if(*badchar != '\0'){
+        printf("Invalid Key\n");
+        return EXIT_FAILURE;
+    }
+
+    if(subs_cipher_ascii(input_file_path, key, mode)){
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
@@ -19,7 +30,7 @@ int check_arguments_lenght(const int argc, const int lenght){
         printf("Too many arguments!\n");
         return EXIT_FAILURE;
     } else if(argc < lenght){
-        printf("File path not provided!\n");
+        printf("Too few arguments!\n");
         return EXIT_FAILURE;
     } 
     return 0;
